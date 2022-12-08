50af04e13a41986ca0c2f37649450945470f4bf8
exitsimulation <jablonski.aaron@gmail.com>
2022-12-08 20:02:04 +0100

    Test redeploy

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import RootLayout from "./layout";
 export default function Home() {
   return (
       <div className={"flex flex-col justify-center items-center h-[100vh]"}>
-          <div className={"text-stone-300 text-2xl uppercase"}>Next.js 13 with tailwind starter</div>
+          <div className={"text-stone-300 text-2xl uppercase"}>Next.js 13 with tailwind starter.</div>
       </div>
   )
 }
